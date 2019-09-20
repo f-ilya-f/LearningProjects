8d70dca8b97bfbe0de240cfcd9946b50e2199af4
Ilya Fominykh <filya-1990@mail.ru>
2019-09-20 22:35:50 +0500

    отформатирован файл

--- sandbox/src/main/java/ru/ivfominy/MyFirstProgram.java
+++ sandbox/src/main/java/ru/ivfominy/MyFirstProgram.java
@@ -1,7 +1,7 @@
 package src.main.java.ru.ivfominy;
 
 public class MyFirstProgram {
-    public static void main(String[] args){
-System.out.println("Hello!");
-}
+    public static void main(String[] args) {
+        System.out.println("Hello!");
+    }
 }
