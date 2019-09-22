def27855fdb6c5c682f23cb0215fa6feb4c4bc92
Ilya Fominykh <filya-1990@mail.ru>
2019-09-22 15:33:00 +0500

    добавлено несколько фукций

--- sandbox/src/main/java/ru/ivfominy/MyFirstProgram.java
+++ sandbox/src/main/java/ru/ivfominy/MyFirstProgram.java
@@ -3,5 +3,19 @@ package src.main.java.ru.ivfominy;
 public class MyFirstProgram {
     public static void main(String[] args) {
         System.out.println("Hello!");
+
+        double len=5;
+        System.out.println("Площадь квадрата со стороной "+len+" = "+area(len));
+
+        double a=4, b=5;
+        System.out.println("Площадь прямоугольника со сторонами "+a+" и "+b+" = "+area(a,b));
+    }
+
+    public static  double area(double l){
+        return  l*l;
+    }
+
+    public  static double area(double a, double b){
+        return  a*b;
     }
 }
