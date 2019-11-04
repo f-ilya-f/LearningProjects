a8926e9ce4ce6b2c8a62cfdb4624879f3937c64a
Ilya Fominykh <filya-1990@mail.ru>
2019-11-04 21:12:30 +0500

    создана возможность выбора браузера

--- addressbook-web-tests/src/test/java/ru/ivfominy/addressbook/appmanager/ApplicationManager.java
+++ addressbook-web-tests/src/test/java/ru/ivfominy/addressbook/appmanager/ApplicationManager.java
@@ -5,6 +5,9 @@ import org.openqa.selenium.NoAlertPresentException;
 import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.ie.InternetExplorerDriver;
+import org.openqa.selenium.remote.BrowserType;
 
 import java.util.concurrent.TimeUnit;
 
@@ -14,9 +17,21 @@ public class ApplicationManager {
     private NavigationHelper navigationHelper;
     private GroupHelper groupHelper;
     private SessionHelper sessionHelper;
+    private String browser;
+
+    public ApplicationManager(String browser) {
+        this.browser = browser;
+    }
 
     public void init() {
-        wd = new ChromeDriver();
+        if (browser==BrowserType.CHROME){
+            wd = new ChromeDriver();
+        } else if (browser==BrowserType.FIREFOX){
+            wd = new FirefoxDriver();
+        } else if (browser==BrowserType.IE){
+            wd=new InternetExplorerDriver();
+        }
+
         wd.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
         wd.get("http://localhost/addressbook/");
         groupHelper = new GroupHelper(wd);
