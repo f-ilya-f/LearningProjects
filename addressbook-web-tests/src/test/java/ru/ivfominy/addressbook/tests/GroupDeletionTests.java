5801e124c4c1eae20105cc85ee7a24f273d87123
Ilya Fominykh <filya-1990@mail.ru>
2019-11-05 21:04:09 +0500

    создаем коллекцию из существующих групп и сравниваем размеры коллекций вместо количества групп

--- addressbook-web-tests/src/test/java/ru/ivfominy/addressbook/tests/GroupDeletionTests.java
+++ addressbook-web-tests/src/test/java/ru/ivfominy/addressbook/tests/GroupDeletionTests.java
@@ -4,20 +4,23 @@ import org.testng.Assert;
 import org.testng.annotations.Test;
 import ru.ivfominy.addressbook.model.GroupData;
 
+import java.util.List;
+
 public class GroupDeletionTests extends TestBase {
 
-  @Test
-  public void testGroupDeletion() throws Exception {
+    @Test
+    public void testGroupDeletion() throws Exception {
         app.getNavigationHelper().gotoGroupPage();
-    int before=app.getGroupHelper().getGroupCount();
-    if (!app.getGroupHelper().isThereAGroup()){
-      app.getGroupHelper().createGroup(new GroupData("test1", null, null));
+        //предусловие на создание группы если список текущих групп пуст
+        if (!app.getGroupHelper().isThereAGroup()) {
+            app.getGroupHelper().createGroup(new GroupData("test1", null, null));
+        }
+      List<GroupData> before = app.getGroupHelper().getGroupList();
+                app.getGroupHelper().selectGroup(0);
+        app.getGroupHelper().deleteSelectedGroups();
+        app.getGroupHelper().returnToGroupPage();
+      List<GroupData> after = app.getGroupHelper().getGroupList();
+        Assert.assertEquals(after.size(), before.size() - 1);
     }
-    app.getGroupHelper().selectGroup(0);
-    app.getGroupHelper().deleteSelectedGroups();
-    app.getGroupHelper().returnToGroupPage();
-    int after=app.getGroupHelper().getGroupCount();
-    Assert.assertEquals(after, before-1);
-  }
 
 }
