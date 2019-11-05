fe865d72751529e6c1daf75d5884afbba61e6de5
Ilya Fominykh <filya-1990@mail.ru>
2019-11-05 22:46:37 +0500

    заменили поиск максимума через цикл на поиск через лямбда функцию

--- addressbook-web-tests/src/test/java/ru/ivfominy/addressbook/tests/GroupCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/ivfominy/addressbook/tests/GroupCreationTests.java
@@ -4,6 +4,7 @@ import org.testng.Assert;
 import org.testng.annotations.Test;
 import ru.ivfominy.addressbook.model.GroupData;
 
+import java.util.Comparator;
 import java.util.HashSet;
 import java.util.List;
 
@@ -21,12 +22,7 @@ public class GroupCreationTests extends TestBase {
         Assert.assertEquals(after.size(), before.size() + 1);
         //Assert.assertEquals(after, before + 1);
 
-        int max = 0;
-        for (GroupData g : after) {
-            if (g.getId() > max)
-                max = g.getId();
-        }
-        newGroup.setId(max);
+        newGroup.setId(after.stream().max((o1, o2) -> Integer.compare(o1.getId(), o2.getId())).get().getId());
         before.add(newGroup);
         Assert.assertEquals(new HashSet<Object>(after), new HashSet<Object>(before));
     }
